01a7a66ae0ce3604d9abf8b10a97432de6aa4901
Yan <yancarlodev@gmail.com>
2025-08-13 20:25:19 -0400

    aula-02

---
+++ app/dist/js/decorator/inspect.js
@@ -0,0 +1,11 @@
+export function inspect(target, propertyKey, descriptor) {
+    const metodoOriginal = descriptor.value;
+    descriptor.value = function (...args) {
+        console.log(`--- Metódo ${propertyKey}`);
+        console.log(`------ parâmetros: ${JSON.stringify(args)}`);
+        const retorno = metodoOriginal.apply(this, args);
+        console.log(`------ retorno: ${JSON.stringify(retorno)}`);
+        return retorno;
+    };
+    return descriptor;
+}
